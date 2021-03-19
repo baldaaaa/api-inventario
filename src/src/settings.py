cfe667101f220708303b9636fcf12d0fab039301
Roberto Balda <80551061+baldaaaa@users.noreply.github.com>
2021-03-19 04:03:23 -0500

    minor changes setting.py
    security measures

--- src/src/settings.py
+++ src/src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')r6a&5-k^*=-_t5q#o34n%cs2g4f6t57zdteq=@-xw*74s37mb'
+SECRET_KEY = #secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
